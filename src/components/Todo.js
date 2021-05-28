2c55d199fa1cf77d7b560e6c7c58c153122dc5ff
Neil Gebhard <neilgebhard@gmail.com>
2021-05-28 14:52:07 +1000

    :bug: Fix a bug, add task edit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,16 +10,27 @@ export default function Todo(props) {
     props.setTasks(updatedTasks);
   }
 
+  function editTask(id) {
+    const name = prompt('What would you like to rename the task to?');
+    const updatedTasks = props.tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, name };
+      }
+      return task;
+    });
+    props.setTasks(updatedTasks);
+  }
+
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-          <input id={props.id} type="checkbox" checked={props.completed} onClick={() => props.toggleComplete(props.id)} />
+          <input id={props.id} type="checkbox" checked={props.completed} onClick={() => toggleComplete(props.id)} />
           <label className="todo-label" htmlFor={props.id}>
               {props.name}
           </label>
       </div>
       <div className="btn-group">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={() => editTask(props.id)}>
               Edit <span className="visually-hidden">{props.name}</span>
           </button>
           <button type="button" className="btn btn__danger" onClick={() => props.deleteTask(props.id)}>
